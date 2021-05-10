3fe0ff377fe250f2ca5969bf132302e852f87bb7
Jack Curtin <jackcurtin94@gmail.com>
2021-05-10 11:27:36 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { LoginComponent } from './login/login.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { SignupComponent } from './signup/signup.component';
+const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'categories',
+    component: CategoriesComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
